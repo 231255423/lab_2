1e06c70f3d2b1fbcb8aa515580215eb90050c675
231255423 <ivan.231255@gmail.com>
2026-05-20 22:51:03 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import Skills from './components/Skills';
+import Profile from './components/Profile';
+import Education from './components/Education';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Profile />
+      <Skills />
+      <Education />
+    </div>
+  );
+}
+
+export default App;
